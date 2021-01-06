1858960e9faaca814cbfcfd23f2e6749b450972c
raulratiu9 <raulratiu2@gmail.com>
2021-01-06 14:43:18 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+//Navigation Header//
+window.addEventListener("scroll", function () {
+	var header = document.querySelector("header");
+	header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+///Hamburger Menu//
+const navSlide = () => {
+	const hamburger = document.querySelector('.hamburger');
+	const nav = document.querySelector('nav ul');
+	const navLinks = document.querySelectorAll('ul li');
+
+	hamburger.addEventListener('click', () => {
+
+		nav.classList.toggle('nav-collapsed');
+
+		navLinks.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = '';
+			} else {
+				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+			}
+        });
+        
+		hamburger.classList.toggle('toggle');
+    });
+    
+}
+
+navSlide();
