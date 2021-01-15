2cf7da884e0089352e1d89ec04f48361673cfb00
raulratiu9 <raulratiu2@gmail.com>
2021-01-15 12:37:26 +0200

    new features

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,9 @@
 //Navigation Header//
-window.addEventListener("scroll", function () {
-	var header = document.querySelector("header");
-	header.classList.toggle("sticky", window.scrollY > 0);
+const hamburger_menu = document.querySelector(".hamburger-menu");
+
+const container = document.querySelector(".container");
+
+hamburger_menu.addEventListener("click", () =>{
+    container.classList.toggle("active");
 })
 
-///Hamburger Menu//
-const navSlide = () => {
-	const hamburger = document.querySelector('.hamburger');
-	const nav = document.querySelector('nav ul');
-	const navLinks = document.querySelectorAll('ul li');
-
-	hamburger.addEventListener('click', () => {
-
-		nav.classList.toggle('nav-collapsed');
-
-		navLinks.forEach((link, index) => {
-			if (link.style.animation) {
-				link.style.animation = '';
-			} else {
-				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-			}
-        });
-        
-		hamburger.classList.toggle('toggle');
-    });
-    
-}
-
-navSlide();
-
-/* Video */
-
-document.getElementById('video').play();
