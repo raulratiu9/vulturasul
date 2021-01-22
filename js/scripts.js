21d4219249b5080b6d7126d5eeb97ee1ba3a67a3
raulratiu9 <raulratiu2@gmail.com>
2021-01-22 10:00:55 +0200

    optimization

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-//Navigation Header//
-const hamburger_menu = document.querySelector(".hamburger-menu");
-
-const container = document.querySelector(".container");
-
-hamburger_menu.addEventListener("click", () =>{
-    container.classList.toggle("active");
-})
-
